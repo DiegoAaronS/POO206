ef45d983525bb93870fc0073691ca8e14603987e
DiegoAaronS <123047151@upq.edu.mx>
2025-05-16 18:29:43 -0600

    Solicitud de datos en Python

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -7,25 +7,31 @@ varias lineas
 en python """
 
 #2. strings
-print("Hola Mau, soy una cadena")
-print('Adios Mau, soy otra cadena')
+#print("Hola Mau, soy una cadena")
+#print('Adios Mau, soy otra cadena')
 
-variable1= "Hola Mau, soy una cadena"
-print(len(variable1))
-print(variable1[2:5])
-print(variable1[2:])
-print(variable1[:5])
+#variable1= "Hola Mau, soy una cadena"
+#print(len(variable1))
+#print(variable1[2:5])
+#print(variable1[2:])
+#print(variable1[:5])
 
 #3. variables
-x= "Diego Samano"
-x= 4
-x= 5.76
-print(x)
+#x= "Diego Samano"
+#x= 4
+#x= 5.76
+#print(x)
 
-x= int(3)
-y= float(3)
-z= str(3)
-print(x,y,z)
-print(type(x))
-print(type(y))
-print(type(z))
+#x= int(3)
+#y= float(3)
+#z= str(3)
+#print(x,y,z)
+#print(type(x))
+#print(type(y))
+#print(type(z))
+
+#4. solicitud de datos
+a= input("Introduce cualquier dato: ")
+b= int(input("Introduce un numero entero: "))
+c= float(input("Introduce un numero decimal: "))
+print(a,b,c)
