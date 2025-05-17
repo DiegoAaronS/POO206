4185aa3a6ea07b30b1d398c5ae4b4d3ff16b3310
DiegoAaronS <123047151@upq.edu.mx>
2025-05-16 18:19:36 -0600

    Variables en Python

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -9,3 +9,23 @@ en python """
 #2. strings
 print("Hola Mau, soy una cadena")
 print('Adios Mau, soy otra cadena')
+
+variable1= "Hola Mau, soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3. variables
+x= "Diego Samano"
+x= 4
+x= 5.76
+print(x)
+
+x= int(3)
+y= float(3)
+z= str(3)
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
