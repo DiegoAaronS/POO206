5e73d7ecfd932d247834ff57110f09408c8fab3e
DiegoAaronS <123047151@upq.edu.mx>
2025-05-22 19:31:07 -0600

    ParImpar

---
+++ Practica3/ParImar.py
@@ -0,0 +1,10 @@
+while True:
+ try:
+    numero = (int(input("Introduce un número: ")))
+    if numero % 2 == 0:
+        resultado = "par"
+    else:
+        resultado = "impar"
+    print(f"El número {numero} es {resultado}.")
+ except ValueError:
+    print("Error: Debes ingresar un número entero válido.")
